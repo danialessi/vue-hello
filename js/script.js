45cb005784b66558c6d252c6b80315ed04259220
danialessi <daniele.alessi@hotmail.it>
2021-04-19 17:07:36 +0200

    modificata funzione

--- js/script.js
+++ js/script.js
@@ -4,18 +4,27 @@ var app = new Vue (
         data: {
             message: 'Ciao',
             sub_message: 'sono Vue!',
-            color: 'color',
+            color: 'green',
             imagePath: 'img/logo.png',
-            background: 'indigo'
+            love: ''
         },
         methods: {
-            changeColor: function () {
-                if (this.background == 'indigo') {
-                    this.background = 'yellow';
-                } else if (this.background == 'yellow') {
-                    this.background = 'white';
+            changeFramework: function () {
+                if (this.sub_message == 'sono Vue!') {
+                    this.sub_message = 'sono Angular!';
+                    this.imagePath = 'img/angular.png';
+                    this.color = 'red';
+                    this.love = 'but I love Vue!';
+                } else if (this.sub_message == 'sono Angular!') {
+                    this.sub_message = 'sono React!';
+                    this.imagePath = 'img/react.png';
+                    this.color = 'azzurro';
+                    this.love = 'but I love Vue!';
                 } else {
-                    this.background = 'indigo';
+                    this.sub_message = 'sono Vue!';
+                    this.color = 'green';
+                    this.imagePath = 'img/logo.png';
+                    this.love = '';
                 }
             }
         }
